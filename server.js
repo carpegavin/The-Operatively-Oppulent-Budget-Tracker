6088a6d5ee8766565eab0824be78f66c9640cc76
Gavin Phill <carpe.gavin@gmail.com>
2021-03-01 17:37:37 -0600

    process.env attempts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+// const PORT = process.env.PORT || 3000;
 const PORT = 3000;
 
 const app = express();
@@ -20,6 +20,12 @@ mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+//  mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { 
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+//  });
+
 
 // routes
 app.use(require("./routes/api.js"));
